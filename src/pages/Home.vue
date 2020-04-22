1ffee41519c746a9498fa0eeb37e6a5804beac00
zhaolinjie <zhaolinjie@qq.com>
2020-04-22 17:58:58 +0800

    项目删减

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>123456</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="less">
+</style>
