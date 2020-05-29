f5e9b9debc544fe24cfc493631979498d44ab650
zhaolinjie <zhaolinjie@qq.com>
2020-05-29 18:02:04 +0800

    updata

--- src/components/lattice.vue
+++ src/components/lattice.vue
@@ -3,14 +3,22 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { CreateElement } from "vue";
 
+interface state {
+  id: String;
+  row: Number;
+  column: Number;
+  opened: Boolean;
+  flagged: Boolean;
+  mined: Boolean;
+  neighborMineCount: Number;
+}
+
 @Component
 export default class HelloWorld extends Vue {
   // @Prop() 类似 props: { msg: { type: Number }, propB: { default: 'default value' },  }
   // @Prop({ default: "default value" }) readonly propB!: string;
 
-  protected state = {
-    status: ""
-  };
+  protected state!: state;
 
   private created(): void {
     console.log("组件创建好了");
@@ -26,11 +34,12 @@ export default class HelloWorld extends Vue {
 <style scoped lang="less">
 .box {
   border: 2px solid #ddd;
-  width: 50vw;
-  height: 50vh;
-  margin: auto;
-  .start {
-    text-align: center;
+  width: 15px;
+  height: 15px;
+  background-color: #fff;
+  cursor: pointer;
+  &:hover {
+    background-color: azure;
   }
 }
 </style>
