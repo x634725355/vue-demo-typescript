155271d67f42817ad674cf85e4de34abb20c1b61
zhaolinjie <zhaolinjie@qq.com>
2020-05-29 17:05:22 +0800

    updata

--- src/components/lattice.vue
+++ src/components/lattice.vue
@@ -8,38 +8,17 @@ export default class HelloWorld extends Vue {
   // @Prop() 类似 props: { msg: { type: Number }, propB: { default: 'default value' },  }
   // @Prop({ default: "default value" }) readonly propB!: string;
 
-  protected count: number = 0;
-
-  protected idCount: number = 0;
+  protected state = {
+    status: ""
+  };
 
   private created(): void {
     console.log("组件创建好了");
   }
 
-  get messageCount(): string {
-    return "现在的数字" + this.count;
-  }
-
-  increment() {
-    this.count++;
-  }
-
-  decrement() {
-    this.count--;
-  }
-
   // jsx
   render(h: CreateElement) {
-    const { decrement, messageCount, increment } = this;
-
-    return (
-      <div class="box">
-        <div class="start">Game Start</div>
-        <div>{messageCount}</div>
-        <button onclick={increment}>++++</button>
-        <button onclick={decrement}>-----</button>
-      </div>
-    );
+    return <div class="box"></div>;
   }
 }
 </script>
